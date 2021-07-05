69c2f918b058fab4039a48d004a992b7246c8804
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2021-07-04 23:56:37 -0300

    testes da loucura

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,73 @@
-import { Container, Content, FormContent, InputContainer } from '../styles/pages/HomeStyles';
-import Logo from '../assets/logo.svg';
-import Illustration from '../assets/illustration.svg';
-import { useState } from 'react';
+import {
+  Container,
+  Content,
+  FormContent,
+  InputContainer,
+} from "../styles/pages/HomeStyles";
+import Logo from "../assets/logo.svg";
+import Illustration from "../assets/illustration.svg";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
-  const [formType, setFormType] = useState('join');
+  const { socket } = useSelector((state) => state.socket);
 
-  const [nickname, setNickname] = useState('');
-  const [roomCode, setRoomCode] = useState('');
-  const [videoUrl, setVideoUrl] = useState('');
+  const [formType, setFormType] = useState("join");
+
+  const [nickname, setNickname] = useState("");
+  const [roomCode, setRoomCode] = useState("");
+  const [videoUrl, setVideoUrl] = useState("");
+
+  const handleCreateRoom = async (e) => {
+    e.preventDefault();
+    if (nickname && roomCode)
+      socket?.emit("createRoom", { name: nickname, roomName: roomCode });
+  };
+
+  const handleJoinRoom = async (e) => {
+    e.preventDefault();
+    if (nickname && roomCode)
+      socket?.emit("joinRoom", { name: nickname, roomName: roomCode });
+  };
 
   return (
     <Container>
       <Logo />
 
       <Content>
-        <h1>Assista vídeos do Youtube <span>simultaneamente</span> de forma <span>fácil e pratica!</span></h1>
+        <h1>
+          Assista vídeos do Youtube <span>simultaneamente</span> de forma{" "}
+          <span>fácil e pratica!</span>
+        </h1>
 
         <div>
           <FormContent>
-            {formType === 'join' ? (
+            {formType === "join" ? (
               <>
                 <h2>entrar em uma sala existente</h2>
 
                 <InputContainer hasValue={!!nickname}>
                   <label htmlFor="nickname">Escolha seu nick</label>
-                  <input type="text" id="nickname" onChange={({ target }) => setNickname(target.value)} />
+                  <input
+                    type="text"
+                    id="nickname"
+                    onChange={({ target }) => setNickname(target.value)}
+                  />
                 </InputContainer>
                 <InputContainer hasValue={!!roomCode}>
                   <label htmlFor="roomCode">Código da sala</label>
-                  <input type="text" id="roomCode" onChange={({ target }) => setRoomCode(target.value)} />
+                  <input
+                    type="text"
+                    id="roomCode"
+                    onChange={({ target }) => setRoomCode(target.value)}
+                  />
                 </InputContainer>
 
-                <button>Entrar</button>
-                <p>Ou crie a sua <b onClick={() => setFormType('create')}>clicando aqui</b></p>
+                <button onClick={handleJoinRoom}>Entrar</button>
+                <p>
+                  Ou crie a sua{" "}
+                  <b onClick={() => setFormType("create")}>clicando aqui</b>
+                </p>
               </>
             ) : (
               <>
@@ -41,15 +75,26 @@ export default function Home() {
 
                 <InputContainer hasValue={!!nickname}>
                   <label htmlFor="nickname">Escolha seu nick</label>
-                  <input type="text" id="nickname" onChange={({ target }) => setNickname(target.value)} />
+                  <input
+                    type="text"
+                    id="nickname"
+                    onChange={({ target }) => setNickname(target.value)}
+                  />
                 </InputContainer>
-                <InputContainer hasValue={!!videoUrl}>
-                  <label htmlFor="roomCode">URL do vídeo</label>
-                  <input type="text" id="roomCode" onChange={({ target }) => setVideoUrl(target.value)} />
+                <InputContainer hasValue={!!roomCode}>
+                  <label htmlFor="roomCode">Nome da sala</label>
+                  <input
+                    type="text"
+                    id="roomCode"
+                    onChange={({ target }) => setRoomCode(target.value)}
+                  />
                 </InputContainer>
 
-                <button>Criar</button>
-                <p>Ou entre em uma existente <b onClick={() => setFormType('join')}>clicando aqui</b></p>
+                <button onClick={handleCreateRoom}>Criar</button>
+                <p>
+                  Ou entre em uma existente{" "}
+                  <b onClick={() => setFormType("join")}>clicando aqui</b>
+                </p>
               </>
             )}
           </FormContent>
@@ -57,5 +102,5 @@ export default function Home() {
         </div>
       </Content>
     </Container>
-  )
+  );
 }
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -22,6 +22,10 @@ const connect = () => {
       socketIo.on("disconnect", () => {
         dispatch(setSocket(null));
       });
+
+      socketIo.on("roomUpdate", (data) => {
+        console.log(data);
+      });
     }
   } catch (error) {
     console.error(error);
